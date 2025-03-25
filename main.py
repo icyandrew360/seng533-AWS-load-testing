24c71f9153f01b25b0541a2d5e7792995906964e
Andrew Howe <andrewhowe1000@gmail.com>
2025-03-25 00:58:58 -0600

    timing of uploads to the standard s3 class

--- main.py
+++ main.py
@@ -22,6 +22,33 @@ def generate_test_files_for_upload(ssh):
 
         print(f"Time taken; {end_time - start_time:.2f} seconds\n")
 
+def time_and_upload_files_to_s3(ssh):
+    upload_files_commands = {
+        "upload 1mb file to standard storage": "aws s3 cp test-1mb.file s3://seng533-standard-storage-group15/test-upload-1mb-agent1.file",
+        "upload 100mb file to standard storage": "aws s3 cp test-10mb.file s3://seng533-standard-storage-group15/test-upload-10mb-agent1.file",
+    }
+
+    print("Timing of PUT commands:")
+
+    total_time = 0
+    total_size_mb = 0
+    for desc, command in upload_files_commands.items():
+        print(f"Executing command: {desc}")
+        start_time = time.time()
+        stdin, stdout, stderr = ssh.exec_command(command)
+        stdout.channel.recv_exit_status()
+        end_time = time.time()
+
+        print(f"Time taken; {end_time - start_time:.2f} seconds\n")
+        total_time += (end_time - start_time)
+        total_size_mb += int(desc.split()[1][:-2])
+    print("Uploads complete.")
+
+    tput = total_size_mb / total_time
+    print(f"total size: {total_size_mb} MB")
+    print(f"total time: {total_time:.2f} seconds")
+    print(f"total throughput: {tput:.2f} MB/s")
+
 
 def main():
     ssh = paramiko.SSHClient()
@@ -30,7 +57,9 @@ def main():
     print("Connected to EC2 instance")
 
     generate_test_files_for_upload(ssh)
-    
+
+    time_and_upload_files_to_s3(ssh)
+
     ssh.close()
 
 if __name__ == "__main__":
